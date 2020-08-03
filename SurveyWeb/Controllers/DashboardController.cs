9d59ea81b8f9269a7eff40a5d1fff5eba4b9b032
Murilo Barros <muriloxd02@gmail.com>
2020-08-03 15:59:05 -0300

    graficos-aspnet

---
+++ SurveyWeb/Controllers/DashboardController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SurveyWeb.Filters;
+using cl = Survey.Controllers;
+
+namespace SurveyWeb.Controllers
+{
+    [ValidarUsuario]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ObterQuestionariosInicio(int Ano)
+        {
+            int idUsuario = int.Parse(Request.Cookies["idUsuario"].ToString());
+            var q = new cl.QuestionarioController().ObterPorUsuario(idUsuario);
+            var dadosGrafico = (from x in q
+                                where x.Inicio.Year == Ano
+                                group x by new { x.Inicio.Month } into g
+                                select new
+                                {
+                                    Mes = g.Key.Month,
+                                    Quantidade = g.Count()
+                                }).ToList();
+            if (dadosGrafico.Count > 0)
+                return Json(dadosGrafico);
+            else
+                return Json("");
+        }
+
+        public ActionResult ObterQuestionariosFim(int Ano)
+        {
+            int idUsuario = int.Parse(Request.Cookies["idUsuario"].ToString());
+            var q = new cl.QuestionarioController().ObterPorUsuario(idUsuario);
+            var dadosGrafico = (from x in q
+                                where x.Fim.Year == Ano
+                                group x by new { x.Fim.Month } into g
+                                select new
+                                {
+                                    Mes = g.Key.Month,
+                                    Quantidade = g.Count()
+                                }).ToList();
+            if (dadosGrafico.Count > 0)
+                return Json(dadosGrafico);
+            else
+                return Json("");
+        }
+
+    }
+}
---
+++ SurveyWeb/wwwroot/js/Views/Dashboard.js
@@ -0,0 +1,115 @@
+﻿function GraficoPizza(x) {
+    var valores = [];
+    var cores = [];
+    var legendas = [];
+    for (i = 0; i < $(x).length; i++) {
+        valores.push(Number(x[i].quantidade));
+        legendas.push("Mês: " + x[i].mes);
+        cores.push('#' + Math.floor(Math.random() * 16777215).toString(16));
+    }
+    var config = {
+        type: 'pie',
+        data: {
+            datasets: [{
+                data: valores,
+                backgroundColor: cores
+            }],
+            labels: legendas
+        },
+        options: {
+            resposive: true,
+            legend: {
+                display: true
+            },
+            tooltips: {
+                enabled: true
+            }
+        }
+    };
+
+    if (typeof meuGraficoPizza !== 'undefined') {
+        meuGraficoPizza.destroy();
+    }
+
+    var g = document.getElementById("grafPizza").getContext("2d");
+    meuGraficoPizza = new Chart(g, config);
+}
+
+
+function GraficoBarra(x) {
+    var valores = [];
+    var cores = [];
+    var legendas = [];
+    for (i = 0; i < $(x).length; i++) {
+        valores.push(Number(x[i].quantidade));
+        legendas.push("Mês: " + x[i].mes);
+        cores.push('#' + Math.floor(Math.random() * 16777215).toString(16));
+    }
+    var config = {
+        type: 'bar',
+        data: {
+            datasets: [{
+                data: valores,
+                backgroundColor: cores
+            }],
+            labels: legendas
+        },
+        options: {
+            resposive: true,
+            scales: {
+                xAxes: [{
+                    stacked: true
+                }],
+                yAxes: [{
+                    stacked: true
+                }]
+            },
+            legend: {
+                display: false
+            },
+            tooltips: {
+                enabled: true
+            }
+        }
+    };
+
+    if (typeof meuGraficoBarra !== 'undefined') {
+        meuGraficoBarra.destroy();
+    }
+
+    var g = document.getElementById("grafBarra").getContext("2d");
+    meuGraficoBarra = new Chart(g, config);
+}
+
+
+function MontarGrafico(a) {
+    $.ajax({
+        cache: false,
+        url: "/Dashboard/ObterQuestionariosInicio",
+        data: { Ano: a },
+        success: function (retorno) {
+            GraficoPizza(retorno);
+        },
+        error: function (req, status, error) {
+            alert(error);
+        }
+    });
+
+    $.ajax({
+        cache: false,
+        url: "/Dashboard/ObterQuestionariosFim",
+        data: { Ano: a },
+        success: function (retorno) {
+            GraficoBarra(retorno);
+        },
+        error: function (req, status, error) {
+            alert(error);
+        }
+    });
+
+}
+
+window.onload = function () {
+    var d = new Date();
+    MontarGrafico(d.getFullYear());
+}
